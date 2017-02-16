dab523c14a5d48ba444c19bc640d93b46c7998e5
Mike Gee <geezyx@gmail.com>
2017-02-16 11:50:38 -0500

    [fix] hack for travis-ci rubygems build

--- .travis/is_valid_tag.rb
+++ .travis/is_valid_tag.rb
@@ -5,8 +5,8 @@ require_relative "../lib/whatmask.rb"
 travis_tag = ARGV[0]
 
 if Whatmask::VERSION != travis_tag then
-    STDERR.puts "Tag \"#{travis_tag}\" does not match version.rb (#{Whatmask::VERSION})"
-    exit 1
+  STDERR.puts "Tag \"#{travis_tag}\" does not match version.rb (#{Whatmask::VERSION})"
+  exit 1
 else
-    exit 0
-end 
+  exit 0
+end
